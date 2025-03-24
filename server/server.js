7c75c6aaacf3b8be334313919c48604b3ee5f5a5
Innowidetech <innowidetech@gmail.com>
2025-03-24 15:38:08 +0530

    server

--- server/server.js
+++ server/server.js
@@ -26,7 +26,20 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173","https://ecommerce-web03.web.app","https://suparcart.web.app"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json({ limit: '15mb' }));
